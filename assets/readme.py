0efc50c2e9169275b50afa77f7c912ac667a6b76
Mario <mario@fokken.cc>
2025-08-29 16:22:54 +0200

    Also replace the Chuck
    Updated the about_me list to replace 'Chuck' with 'Mario' as well.

--- assets/readme.py
+++ assets/readme.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as file: readme = file.readlines()
 
 with open("assets/chuck.txt", "r", encoding="utf8") as file: chuck_lines = file.readlines()
 
-about_me = [l.replace("Chuck Norris", "Mario").strip() for l in random.sample(chuck_lines, 3)]
+about_me = [l.replace("Chuck Norris", "Mario").replace("Chuck", "Mario").strip() for l in random.sample(chuck_lines, 3)]
 
 readme[7] = f"- {about_me[0]}\n"
 readme[8] = f"- {about_me[1]}\n"
@@ -13,5 +13,6 @@ readme[9] = f"- {about_me[2]}\n"
 response = requests.get("https://raw.githubusercontent.com/Torfkopp/LapisDiscordBot/refs/heads/master/resources/wisdom.txt")
 readme[14] = "> " + random.choice(response.text.split('\n')) + "\n"
 with open("README.md", "w", encoding="utf8") as file: file.writelines(readme)
+
 
 
