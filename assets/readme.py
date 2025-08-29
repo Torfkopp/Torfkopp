484fb0201b10dccb0e103234ee81efe57a75b8c8
Mario <mario@fokken.cc>
2025-08-29 14:21:48 +0200

    Update README formatting and content insertion

--- assets/readme.py
+++ assets/readme.py
@@ -9,10 +9,10 @@ for i in range(3):
     l = random.choice(chuck_lines).strip()
     about_me.append(l.replace("Chuck Norris", "Mario"))
 
-readme[34] = f"<li>{about_me[0]}</li>\n"
-readme[35] = f"<li>{about_me[1]}</li>\n"
-readme[36] = f"<li>{about_me[2]}</li>\n"
+readme[7] = f"- {about_me[0]}\n"
+readme[8] = f"- {about_me[1]}\n"
+readme[9] = f"- {about_me[2]}\n"
 
 response = requests.get("https://raw.githubusercontent.com/Torfkopp/LapisDiscordBot/refs/heads/master/resources/wisdom.txt")
-readme[54] = random.choice(response.text.split("\n")) + "\n"
+readme[14] = "> *" + random.choice(response.text.split('\n')) + "*\n"
 with open("../README.md", "w", encoding="utf8") as file: file.writelines(readme)
